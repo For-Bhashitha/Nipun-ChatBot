774dfafcae1e7913845ac7c7d7f5c54f83914674
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-11-17 11:41:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ START_BUTTON = InlineKeyboardMarkup(
 @KINGAMDA.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     await update.reply_photo(
-        START_IMG,
+        PM_START_IMG,
         caption=PM_START_TEXT,
         reply_markup=InlineKeyboardMarkup(buttons),
         parse_mode=ParseMode.MARKDOWN,
